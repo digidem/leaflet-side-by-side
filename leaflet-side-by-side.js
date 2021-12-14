aecff94bd459f55d09a279b94232c3b5c0e8ee0d
owlegodinho <77276690+owlegodinho@users.noreply.github.com>
2021-12-14 10:02:10 +0000

    Changed getContainer to getPane
    Changed getContainer to getPane in order to have compatibility with image overlays

--- leaflet-side-by-side.js
+++ leaflet-side-by-side.js
@@ -95,10 +95,10 @@ L.Control.SideBySide = L.Control.extend({
       return this
     }
     if (this._leftLayer) {
-      this._leftLayer.getContainer().style.clip = ''
+      this._leftLayer.getPane().style.clip = ''
     }
     if (this._rightLayer) {
-      this._rightLayer.getContainer().style.clip = ''
+      this._rightLayer.getPane().style.clip = ''
     }
     this._removeEvents()
     L.DomUtil.remove(this._container)
@@ -132,10 +132,10 @@ L.Control.SideBySide = L.Control.extend({
     var clipLeft = 'rect(' + [nw.y, clipX, se.y, nw.x].join('px,') + 'px)'
     var clipRight = 'rect(' + [nw.y, se.x, se.y, clipX].join('px,') + 'px)'
     if (this._leftLayer) {
-      this._leftLayer.getContainer().style.clip = clipLeft
+      this._leftLayer.getPane().style.clip = clipLeft
     }
     if (this._rightLayer) {
-      this._rightLayer.getContainer().style.clip = clipRight
+      this._rightLayer.getPane().style.clip = clipRight
     }
   },
 
